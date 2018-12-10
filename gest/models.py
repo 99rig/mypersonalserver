249710d18a681200e33b7f2d1563ff42328a456b
99rig <unblockchain@gmail.com>
2018-12-10 21:21:06 +0100

    Inserito App Gest....bisogna migrare il model

--- gest/models.py
+++ gest/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+############ ANAGRAFICA ########
+
+class Anagrafica(models.Model):
+    data_creazione = models.DateField(default=timezone.now)
+    nome = models.CharField(max_length=100)
+    cognome = models.CharField(max_length=100)
+    indirizzo = models.CharField(max_length=100)
+    citta = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=30)
+    email = models.EmailField(max_length=40, verbose_name='E-mail')
+    note = models.TextField(max_length=500, null=True, blank=True)
+
+
+    def nuovo(self):
+        self.data_creazione = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return u'%s %s' % (self.nome ,self.cognome)
+
+
+
+    class Meta:
+        verbose_name_plural = "Aangrafiche"
