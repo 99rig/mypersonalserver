de4a11e31a6843d4d8afa4d9b6d968b29ae65025
99rig <unblockchain@gmail.com>
2018-12-11 10:53:36 +0100

    Creazione del modello dei server

--- gest/admin.py
+++ gest/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import ServDomini
 
-# Register your models here.
+class ServDominiAdmin(admin.ModelAdmin):
+    list_display = ("serv_dominio","indirizzo_ip",'nickname_serv','hostname_serv','user_psw',)
+    fieldsets = [
+        ('Server', {
+            'fields': ['serv_dominio', 'indirizzo_ip','nickname_serv','hostname_serv','user_psw']
+        }),
+    ]
+
+
+admin.site.register(ServDomini,ServDominiAdmin)
--- gest/models.py
+++ gest/models.py
@@ -2,27 +2,19 @@ from django.db import models
 from django.utils import timezone
 
 
-############ ANAGRAFICA ########
+############ Server e Domini ########
 
-class Anagrafica(models.Model):
-    data_creazione = models.DateField(default=timezone.now)
-    nome = models.CharField(max_length=100)
-    cognome = models.CharField(max_length=100)
-    indirizzo = models.CharField(max_length=100)
-    citta = models.CharField(max_length=50)
-    telefono = models.CharField(max_length=30)
-    email = models.EmailField(max_length=40, verbose_name='E-mail')
-    note = models.TextField(max_length=500, null=True, blank=True)
-
-
-    def nuovo(self):
-        self.data_creazione = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return u'%s %s' % (self.nome ,self.cognome)
-
-
-
-    class Meta:
-        verbose_name_plural = "Aangrafiche"
+class ServDomini(models.Model):
+    data_creazione  = models.DateField(default=timezone.now)
+    serv_dominio_choise = (
+    ('---','---'),
+    ('VPS','VPS'),
+    ('dominio', 'Dominio'),
+    ('server Fisico','Server Fisico'),
+    ('server virtuale','Server Virtuale'),
+    )
+    serv_dominio = models.CharField(max_length=12, choices=serv_dominio_choise, default='1')
+    indirizzo_ip = models.CharField(max_length=20, null=True, blank=True)
+    nickname_serv = models.CharField(max_length=35, null=True, blank=True)
+    hostname_serv = models.CharField(max_length=35, null=True, blank=True)
+    user_psw = models.TextField(max_length=250, null=True, blank=True)
